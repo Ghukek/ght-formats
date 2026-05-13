e52648eafef73077fe3a67f130d4eaaafc491596
Ghukek <ghukek@gmail.com>
2026-05-12 20:14:42 -0400

    Clarified reason for second run of USFM creation.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 echo "Starting Python tasks..."
 
-python3 export_usfm.py ../concordance.db 1
+python3 export_usfm.py ../concordance.db 1 # Run once with paragraph separation to allow for osis creation.
 
 #for f in ./ght_usfm/*.usfm; do
 #  base=$(basename "$f" .usfm)
@@ -17,7 +17,7 @@ python3 export_usfm.py ../concordance.db 1
 python3 ./u2o/u2o.py -l en GHT ./ght_usfm/*.usfm -o ./GHT.osis.xml
 python3 ./u2o/u2o.py -l grc GHTG ./ghtg_usfm/*.usfm -o ./GHTG.osis.xml
 
-python3 export_usfm.py ../concordance.db
+python3 export_usfm.py ../concordance.db # Run a second time without.
 python3 convert_usx.py 
 python3 eswordexport.py
 
